21e240358d78de8fed3d09398efadc773a0bb3aa
firesmr11 <jimmy9794@naver.com>
2025-09-26 00:48:36 +0900

    First commit

---
+++ src/main/java/com/example/demo/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//JpaRepository를 상속하도록 수정
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+ // JpaRepository<엔티티 이름, 해당 엔티티의 ID 필드 타입>
+}
