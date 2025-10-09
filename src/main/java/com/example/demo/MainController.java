6446b2a7ef507ddb49438343165f5ed117078026
firesmr11 <jimmy9794@naver.com>
2025-10-09 15:34:11 +0900

    practice

---
+++ src/main/java/com/example/demo/MainController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class MainController {
+	@GetMapping("/sbb")
+	@ResponseBody
+	public String index() {
+		return "안녕하신가 sbb에";
+		
+	}
+	@GetMapping("/")
+	public String root() {
+		return "redirect:/question/list";
+		
+	}
+}
