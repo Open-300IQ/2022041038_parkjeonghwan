259a9fb8f7dc5987e20f81ff651b5ee35e30db42
firesmr11 <jimmy9794@naver.com>
2025-10-16 22:42:26 +0900

    practice

---
+++ sampleproject/src/sampleproject/Hello2030.java
@@ -0,0 +1,9 @@
+package sampleproject;
+
+public class Hello2030 {
+   public static void main(String []args ) {
+	   int n= 2030;
+	   System.out.print("헤레렐로 "+ n);
+	   
+   }
+}
---
+++ sampleproject/src/sampleproject/Polygon.java
@@ -0,0 +1,32 @@
+package sampleproject;
+public class Polygon {
+	String shape;
+	int width,height;
+	double area;
+	
+	Polygon(String shape, int width){
+		this.shape=shape;
+		this.width=width;		
+	}
+	
+	Polygon(String shape, int width, int height){
+		this(shape, width);	
+		this.height=height;
+	}
+	//정사각형
+	void calcArea(int width) {
+		area=width*width;		
+	}
+    //삼각형, 직사각형
+	void calcArea(int width,int height,int s) {
+		if(s==1)
+		    area=(width*height)/2.0;
+		else if(s==2)
+			area=width*height;
+	}
+	//출력
+	void printArea() {
+		System.out.println(shape);
+		System.out.println("area"+area);
+	}
+}
