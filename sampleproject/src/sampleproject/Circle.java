fbf2dd4329148e12dd95115778850dd77abc085b
firesmr11 <jimmy9794@naver.com>
2025-11-29 04:07:11 +0900

    practice

--- sampleproject/src/sampleproject/Circle.java
+++ sampleproject/src/sampleproject/Circle.java
@@ -1,4 +1,4 @@
-package sampleproject;
+package Lecture4;
 
 public class Circle {
 	int radius;
@@ -16,13 +16,12 @@ public class Circle {
 		pizza.radius = 10;
 		pizza.name = "자바피자";
 		double area = pizza.getArea();
-		System.out.println(pizza.name+"의 면적은 " + area);
+		System.out.println(pizza.name + "의 면적은" + area);
 		
 		Circle donut = new Circle();
-		donut.radius=2;
-		donut.name="자바도넛";
+		donut.radius= 2;
+		donut.name = "자바도넛";
 		area = donut.getArea();
-		System.out.println(donut.name + "의 면적은 "+area);
-		
+		System.out.println(donut.name + "의 면적은" + area);
 	}
 }
